4acae342f0b609c190581b6e4ec226a3e19f0599
Jimmy Elvin <jimmy.elvin000@gmail.com>
2025-01-31 08:14:08 +0300

    First commit - committing file to repository

---
+++ Variables.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var x = 6
+    // WHILE LOOP
+    while ( x > 3){
+        println("Jimmy da Great !!!")
+        x = x-2
+    }
+    var  a = 1
+    println(message = "Before the loop. a = $a")
+    while(a < 4){
+        println("In the loop.a = $a")
+        a = a + 1
+    }
+}
